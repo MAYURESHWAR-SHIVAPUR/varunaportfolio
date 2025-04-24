6057489da8d1129e8201515562c9bcb82a83303f
Mayureshwar S <mayureshwarshivapur2004@gmail.com>
2025-04-24 21:06:08 +0530

    Update Cards.jsx

--- src/assestes/Projects/Cards.jsx
+++ src/assestes/Projects/Cards.jsx
@@ -3,41 +3,38 @@ import ProjectsCss from './ProjectsCss.module.css'
 
 
 const Cards = (props) => {
+    const { title, first, second, third, Basic_info, ul, moreData, host, git } = props;
 
-    const { title,first,second,third, Basic_info, ul, moreData , host,git } = props;
-    
     return (
-        <>
-            <div className={ProjectsCss.child_1}>
-                <h1 >{title}</h1>
+        <div className={ProjectsCss.child_1}>
+            <h1>{title}</h1>
 
-                <ul>
-                    <li>{first}</li>
-                    <li>{second}</li>
-                    <li>{third}</li>
-                </ul>
+            <ul>
+                <li>{first}</li>
+                <li>{second}</li>
+                <li>{third}</li>
+            </ul>
 
-                <p>{Basic_info}</p>
+            <p>{Basic_info}</p>
+
+            <br />
+            <details className={ProjectsCss.moreDetails}>
+                <summary className="color">More Details</summary>
 
                 <br />
-                <details className={ProjectsCss.moreDetails}>
-                    <summary className='color'>More Detials</summary>
 
-                    <br />
+                <h3 className="color font">Visit Website</h3>
+                {git && <a href={git}>GitHub <span>Click Me!</span></a>}
+                {host && <a href={host}>Hosted <span>Click Me!</span></a>}
 
-                    <h3 className='color , font'>visit website</h3>
-                    <a href={git}>Git Hub <span> Click Me!</span> </a>
-                    <a href={host}>Hosted <span> Click Me!</span></a>
+                <br /><br /><br />
 
-                    <br /><br /><br />
+                <h3 className="color font">Languages Used</h3>
+                {ul}
+                <p>{moreData}</p>
+            </details>
+        </div>
+    );
+};
 
-                    <h3 className='color , font'>Languages Used</h3>
-                    {ul}
-                    <p>{moreData}</p>
-
-                </details>
-            </div>
-        </>
-    )
-}
-export default Cards
+export default Cards;
