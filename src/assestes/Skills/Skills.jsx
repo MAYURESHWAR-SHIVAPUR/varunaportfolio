6d76dbc865c1b2319d467c7f6e25364cb29fd878
Mayureshwar S <mayureshwarshivapur2004@gmail.com>
2025-04-24 21:19:32 +0530

    Update Skills.jsx

--- src/assestes/Skills/Skills.jsx
+++ src/assestes/Skills/Skills.jsx
@@ -1,21 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import SkillCss from './SkillCss.module.css'
 import Cards from './cards'
-import html from './photos/html.png'
-import css from './photos/css.png';
-import js from './photos/js.png'
-import react from './photos/react.png'
-import vite from './photos/vite.png'
-import tailwind from './photos/tailwind.png'
-import express from './photos/express.png'
-import node from './photos/node.png'
-import mondb from './photos/mongodb.png'
-import ejs from './photos/ejs.png'
-import git from './photos/git.png'
-import github from './photos/github.png'
-import ts from './photos/ts.png'
-import java from './photos/java.png'
-import py from './photos/py.png'
 import { useEffect, useState } from 'react';
 import Loading from '../Loading/Loadingpage'
 
@@ -24,7 +9,7 @@ const Skills = () => {
 
   const [loading, setLoading] = useState(true);
 
-  const imgsource = [html, css, js, tailwind, react, vite, express, node, mondb, ejs, git, github, ts, java, py];
+  const imgsource = [];
 
   useEffect(() => {
     let loadedImg = 0;
@@ -48,8 +33,8 @@ const Skills = () => {
     )
   }
 
-  const name = ['HTML', 'CSS', "JS", "TailwindCss", "React", "Vite", 'Express JS', 'NODE JS', "Mongo DB", "EJS", "git", "GitHub", "TypeScript", "JAVA", "Python"];
-  const imgs = [html, css, js, tailwind, react, vite, express, node, mondb, ejs, git, github, ts, java, py];
+  const name = [];
+  const imgs = [];
   const cards = name.map((_, i) => <Cards name={name[i]} imgs={imgs[i]} />);
   return (
     <div className={SkillCss.outer}>
